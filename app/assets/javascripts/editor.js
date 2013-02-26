d61d2a9e07d078781054e615365167bade7a1909
David Santoro <soulnafein@gmail.com>
2013-02-26 15:18:13 +0000

    Lesson 4

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -51,52 +51,135 @@ var MinecraftEditor = MinecraftEditor || {};
   };
 
   proto.initBuffers = function() {
-    // Triangle
-    this.rTri = 0;
-    this.triangleVertexPositionBuffer = GL.createBuffer();
-    GL.bindBuffer(GL.ARRAY_BUFFER, this.triangleVertexPositionBuffer);
+    // Pyramid
+    this.rPyramid = 0;
+    this.pyramidVertexPositionBuffer = GL.createBuffer();
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.pyramidVertexPositionBuffer);
     var vertices = [
+      // Front face
       0.0,  1.0,  0.0,
-      -1.0, -1.0,  0.0,
-      1.0, -1.0,  0.0
+      -1.0, -1.0,  1.0,
+      1.0, -1.0,  1.0,
+      // Right face
+      0.0,  1.0,  0.0,
+      1.0, -1.0,  1.0,
+      1.0, -1.0, -1.0,
+      // Back face
+      0.0,  1.0,  0.0,
+      1.0, -1.0, -1.0,
+      -1.0, -1.0, -1.0,
+      // Left face
+      0.0,  1.0,  0.0,
+      -1.0, -1.0, -1.0,
+      -1.0, -1.0,  1.0
     ];
     GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(vertices), GL.STATIC_DRAW);
-    this.triangleVertexPositionBuffer.itemSize = 3;
-    this.triangleVertexPositionBuffer.numItems = 3;
+    this.pyramidVertexPositionBuffer.itemSize = 3;
+    this.pyramidVertexPositionBuffer.numItems = 12;
 
-    this.triangleVertexColorBuffer = GL.createBuffer();
-    GL.bindBuffer(GL.ARRAY_BUFFER, this.triangleVertexColorBuffer);
+    this.pyramidVertexColorBuffer = GL.createBuffer();
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.pyramidVertexColorBuffer);
     var colors = [
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0
+      // Front face
+      1.0, 0.0, 0.0, 1.0,
+      0.0, 1.0, 0.0, 1.0,
+      0.0, 0.0, 1.0, 1.0,
+      // Right face
+      1.0, 0.0, 0.0, 1.0,
+      0.0, 0.0, 1.0, 1.0,
+      0.0, 1.0, 0.0, 1.0,
+      // Back face
+      1.0, 0.0, 0.0, 1.0,
+      0.0, 1.0, 0.0, 1.0,
+      0.0, 0.0, 1.0, 1.0,
+      // Left face
+      1.0, 0.0, 0.0, 1.0,
+      0.0, 0.0, 1.0, 1.0,
+      0.0, 1.0, 0.0, 1.0
     ];
     GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(colors), GL.STATIC_DRAW);
-    this.triangleVertexColorBuffer.itemSize = 4;
-    this.triangleVertexColorBuffer.numItems = 3;
+    this.pyramidVertexColorBuffer.itemSize = 4;
+    this.pyramidVertexColorBuffer.numItems = 12;
 
-    // Square
-    this.rSquare = 0;
-    this.squareVertexPositionBuffer = GL.createBuffer();
-    GL.bindBuffer(GL.ARRAY_BUFFER, this.squareVertexPositionBuffer);
+    // cube
+    this.rCube = 0;
+    this.cubeVertexPositionBuffer = GL.createBuffer();
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.cubeVertexPositionBuffer);
     vertices = [
-      1.0,  1.0,  0.0,
-      -1.0,  1.0,  0.0,
-      1.0, -1.0,  0.0,
-      -1.0, -1.0,  0.0
+      // Front face
+      -1.0, -1.0,  1.0,
+      1.0, -1.0,  1.0,
+      1.0,  1.0,  1.0,
+      -1.0,  1.0,  1.0,
+
+      // Back face
+      -1.0, -1.0, -1.0,
+      -1.0,  1.0, -1.0,
+      1.0,  1.0, -1.0,
+      1.0, -1.0, -1.0,
+
+      // Top face
+      -1.0,  1.0, -1.0,
+      -1.0,  1.0,  1.0,
+      1.0,  1.0,  1.0,
+      1.0,  1.0, -1.0,
+
+      // Bottom face
+      -1.0, -1.0, -1.0,
+      1.0, -1.0, -1.0,
+      1.0, -1.0,  1.0,
+      -1.0, -1.0,  1.0,
+
+      // Right face
+      1.0, -1.0, -1.0,
+      1.0,  1.0, -1.0,
+      1.0,  1.0,  1.0,
+      1.0, -1.0,  1.0,
+
+      // Left face
+      -1.0, -1.0, -1.0,
+      -1.0, -1.0,  1.0,
+      -1.0,  1.0,  1.0,
+      -1.0,  1.0, -1.0,
     ];
     GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(vertices), GL.STATIC_DRAW);
-    this.squareVertexPositionBuffer.itemSize = 3; this.squareVertexPositionBuffer.numItems = 4;
+    this.cubeVertexPositionBuffer.itemSize = 3; 
+    this.cubeVertexPositionBuffer.numItems = 24;
 
-    this.squareVertexColorBuffer = GL.createBuffer();
-    GL.bindBuffer(GL.ARRAY_BUFFER, this.squareVertexColorBuffer);
-    var colors = []
-    for (var i=0; i < 4; i++) {
-      colors = colors.concat([0.5, 0.5, 1.0, 1.0]);
+    this.cubeVertexColorBuffer = GL.createBuffer();
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.cubeVertexColorBuffer);
+    colors = [
+      [1.0, 0.0, 0.0, 1.0], // Front face
+      [1.0, 1.0, 0.0, 1.0], // Back face
+      [0.0, 1.0, 0.0, 1.0], // Top face
+      [1.0, 0.5, 0.5, 1.0], // Bottom face
+      [1.0, 0.0, 1.0, 1.0], // Right face
+      [0.0, 0.0, 1.0, 1.0]  // Left face
+    ];
+    var unpackedColors = [];
+    for (var i in colors) {
+      var color = colors[i];
+      for (var j=0; j < 4; j++) {
+        unpackedColors = unpackedColors.concat(color);
+      }
     }
-    GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(colors), GL.STATIC_DRAW);
-    this.squareVertexColorBuffer.itemSize = 4;
-    this.squareVertexColorBuffer.numItems = 4;
+    GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(unpackedColors), GL.STATIC_DRAW);
+    this.cubeVertexColorBuffer.itemSize = 4;
+    this.cubeVertexColorBuffer.numItems = 24;
+
+    this.cubeVertexIndexBuffer = GL.createBuffer();
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.cubeVertexIndexBuffer);
+    var cubeVertexIndices = [
+      0, 1, 2,      0, 2, 3,    // Front face
+      4, 5, 6,      4, 6, 7,    // Back face
+      8, 9, 10,     8, 10, 11,  // Top face
+      12, 13, 14,   12, 14, 15, // Bottom face
+      16, 17, 18,   16, 18, 19, // Right face
+      20, 21, 22,   20, 22, 23  // Left face
+    ];
+    GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), GL.STATIC_DRAW);
+    this.cubeVertexIndexBuffer.itemSize = 1;
+    this.cubeVertexIndexBuffer.numItems = 36;
   };
 
   proto.drawScene = function() {
@@ -111,32 +194,38 @@ var MinecraftEditor = MinecraftEditor || {};
 
     mat4.identity(this.mvMatrix);
 
-    // triangle
+    mat4.translate(this.mvMatrix, [0.0, 0.0, -1.0]);
+
+    // pyramid
     mat4.translate(this.mvMatrix, [-1.5, 0.0, -7.0]);
     this.mvPushMatrix();
-    mat4.rotate(this.mvMatrix, degToRad(this.rTri), [0, 1, 0]);
+    mat4.rotate(this.mvMatrix, degToRad(this.rPyramid), [0, 1, 0]);
 
-    GL.bindBuffer(GL.ARRAY_BUFFER, this.triangleVertexPositionBuffer);
-    GL.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, this.triangleVertexPositionBuffer.itemSize, GL.FLOAT, false, 0, 0);
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.pyramidVertexPositionBuffer);
+    GL.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, this.pyramidVertexPositionBuffer.itemSize, GL.FLOAT, false, 0, 0);
 
-    GL.bindBuffer(GL.ARRAY_BUFFER, this.triangleVertexColorBuffer);
-    GL.vertexAttribPointer(this.shaderProgram.vertexColorAttribute, this.triangleVertexColorBuffer.itemSize, GL.FLOAT, false, 0, 0);
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.pyramidVertexColorBuffer);
+    GL.vertexAttribPointer(this.shaderProgram.vertexColorAttribute, this.pyramidVertexColorBuffer.itemSize, GL.FLOAT, false, 0, 0);
     this.setMatrixUniforms();
-    GL.drawArrays(GL.TRIANGLES, 0, this.triangleVertexPositionBuffer.numItems);
-
+    GL.drawArrays(GL.TRIANGLES, 0, this.pyramidVertexPositionBuffer.numItems);
     this.mvPopMatrix();
 
-    // square
+    // cube
     mat4.translate(this.mvMatrix, [3.0, 0.0, 0.0]);
     this.mvPushMatrix();
-    mat4.rotate(this.mvMatrix, degToRad(this.rSquare), [1, 0, 0]);
-    GL.bindBuffer(GL.ARRAY_BUFFER, this.squareVertexPositionBuffer);
-    GL.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, this.squareVertexPositionBuffer.itemSize, GL.FLOAT, false, 0, 0);
+    mat4.rotate(this.mvMatrix, degToRad(this.rCube), [1, 1, 1]);
 
-    GL.bindBuffer(GL.ARRAY_BUFFER, this.squareVertexColorBuffer);
-    GL.vertexAttribPointer(this.shaderProgram.vertexColorAttribute, this.squareVertexColorBuffer.itemSize, GL.FLOAT, false, 0, 0);
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.cubeVertexPositionBuffer);
+    GL.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, this.cubeVertexPositionBuffer.itemSize, GL.FLOAT, false, 0, 0);
+
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.cubeVertexColorBuffer);
+    GL.vertexAttribPointer(this.shaderProgram.vertexColorAttribute, this.cubeVertexColorBuffer.itemSize, GL.FLOAT, false, 0, 0);
+
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.cubeVertexIndexBuffer);
     this.setMatrixUniforms();
-    GL.drawArrays(GL.TRIANGLE_STRIP, 0, this.squareVertexPositionBuffer.numItems);
+
+    GL.drawElements(GL.TRIANGLES, this.cubeVertexIndexBuffer.numItems, GL.UNSIGNED_SHORT, 0);
+
     this.mvPopMatrix();
   }
 
@@ -145,8 +234,8 @@ var MinecraftEditor = MinecraftEditor || {};
     if (this.lastTime != 0) {
       var elapsed = timeNow - this.lastTime;
 
-      this.rTri += (90 * elapsed) / 1000.0;
-      this.rSquare += (75 * elapsed) / 1000.0;
+      this.rPyramid += (90 * elapsed) / 1000.0;
+      this.rCube -= (75 * elapsed) / 1000.0;
     }
     this.lastTime = timeNow;
   };
